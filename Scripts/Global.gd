20a228b23a2c35b50332f9d2d6ab9f0ff5408230
Boris Hall <81907643+myst-6@users.noreply.github.com>
2023-07-08 13:00:52 +0100

    added boss enemy system

--- Scripts/Global.gd
+++ Scripts/Global.gd
@@ -2,6 +2,7 @@ extends Node2D
 
 var rng = RandomNumberGenerator.new()
 var root
+var map: AStarGrid2D
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -11,10 +12,11 @@ func _ready():
 
 var spawned = false
 
-func _process(delta):
+func _process(_delta):
 	if not spawned:
 		spawn_normal_enemies()
 		spawn_boss_enemies()
+		init_navigation()
 		spawned = true
 		
 
@@ -27,7 +29,7 @@ func spawn_normal_enemies():
 		get_tree().root.add_child(normal_enemy_inst)
 
 func spawn_boss_enemies():
-	for i in range(0, 2):
+	for i in range(0, 1):
 		var boss_enemy_inst = load("res://Enemies/boss_enemy.tscn").instantiate()
 		var x: int = round(rng.randf_range(0.0, 1920.0))
 		var y: int = round(rng.randf_range(0.0, 1080.0))
@@ -35,3 +37,19 @@ func spawn_boss_enemies():
 		boss_enemy_inst.position = Vector2(x, y)
 		get_tree().root.add_child(boss_enemy_inst)
 
+var cell_size: Vector2 = Vector2(64, 64)
+
+func closest_point(xy: Vector2):
+	xy -= map.offset
+	var ij: Vector2i = Vector2i(floor(xy.x / cell_size.x), floor(xy.y / cell_size.y))
+	return ij
+
+func init_navigation():
+	map = AStarGrid2D.new()
+	map.region = Rect2i(-100, -100, 200, 200)
+	map.cell_size = cell_size
+	map.default_compute_heuristic = AStarGrid2D.HEURISTIC_MANHATTAN
+	map.default_estimate_heuristic = AStarGrid2D.HEURISTIC_MANHATTAN
+	map.diagonal_mode = AStarGrid2D.DIAGONAL_MODE_ONLY_IF_NO_OBSTACLES
+	map.update()
+	print("pos", map.get_point_position(Vector2i(1, 3)))
--- Scripts/Printer.gd
+++ Scripts/Printer.gd
@@ -2,7 +2,6 @@ extends CharacterBody2D
 
 @export var max_time: float = 2
 @export var threshold: float = 0.1
-@export var bounce: bool = true
 @export var base_move_time: float = 0.5
 @export var multiplier: float = 1000
 @export var ink_speed: int = 200
@@ -17,8 +16,8 @@ func _ready():
 # t: a value between 0 and 1, the progress of the movement
 # return: a multiplier for the velocity to be multiplied by
 # https://www.desmos.com/calculator/z4vglvnbqo to find a,b,c
-var a: float = 0.5
-var b: float = 1.5
+var a: float = 1.0
+var b: float = 1.0
 var c: float = 0.5
 func easing(t: float) -> float:
 	var s: float = 1 - t
@@ -55,12 +54,12 @@ func shoot_ink():
 	ink_inst.rotation = angle
 		
 		
-func bounce_with_clamp(min_length: float, max_length: float):
-	var collision: KinematicCollision2D = get_last_slide_collision()
+func bounce(collision: KinematicCollision2D):
 	var norm: Vector2 = collision.get_normal()
-	var length: float = target_velocity.length()
+	var lengthA: float = max(200, target_velocity.length())
+	var lengthB: float = max(200, collision.get_collider_velocity().length())
+	var length: float = sqrt(lengthA * lengthB)
 	var dir: Vector2 = target_velocity.bounce(norm).normalized()
-	length = clamp(length, min_length, max_length)
 	target_velocity = dir * length
 
 func _physics_process(delta):
@@ -68,8 +67,9 @@ func _physics_process(delta):
 	velocity = target_velocity * easing(t)
 	if move_time > 0:
 		move_time -= delta
-	if move_and_slide() and bounce:
-		bounce_with_clamp(200, 2000)
+	var collision: KinematicCollision2D = move_and_collide(velocity * delta)
+	if collision != null:
+		bounce(collision)
 
 
 	
